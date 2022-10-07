7d786b0eaa8dc9855daa0ff1efc2e578dc38e4cf
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-10-07 14:00:10 +0200

    Renamed functions and variables

--- src/main/java/com/costardstudio/patterns/features/observer/controller/SimpleCalculator.java
+++ src/main/java/com/costardstudio/patterns/features/observer/controller/SimpleCalculator.java
@@ -5,33 +5,55 @@ import com.costardstudio.patterns.features.observer.view.InputScreen;
 import com.costardstudio.patterns.features.observer.view.ResultScreen;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SimpleCalculator {
 
     JPanel calculatorPanel;
     CalculatorService calculatorService = new CalculatorService();
+    private final JPanel screenPanel;
 
+    //Constructor
     public SimpleCalculator() {
         calculatorPanel = new JPanel();
+        screenPanel = new JPanel();
+
         NumericButtonController oneButton = new NumericButtonController(this.calculatorService, "1");
         NumericButtonController twoButton = new NumericButtonController(this.calculatorService, "2");
         AdditionButtonController plusButton = new AdditionButtonController(this.calculatorService);
         ResultButtonController equalButton = new ResultButtonController(this.calculatorService);
 
-        InputScreen currentInputLabel = new InputScreen();
-        ResultScreen resultLabel = new ResultScreen();
-        calculatorService.addObserver(currentInputLabel);
-        calculatorService.addObserver(resultLabel);
+        calculatorPanel.add(screenPanel);
 
-        calculatorPanel.add(currentInputLabel.getLabel());
-        calculatorPanel.add(resultLabel.getLabel());
         calculatorPanel.add(oneButton.getActionView().getButton());
         calculatorPanel.add(twoButton.getActionView().getButton());
         calculatorPanel.add(plusButton.getActionView().getButton());
         calculatorPanel.add(equalButton.getActionView().getButton());
+
+        configureScreenPanel();
+
     }
 
     public JPanel getCalculatorPanel() {
         return calculatorPanel;
+    }
+
+    public void configureScreenPanel(){
+        GridLayout screenGridLayout = new GridLayout( 2,1);
+        configureInputScreen();
+        configureResultScreen();
+        this.screenPanel.setLayout(screenGridLayout);
+    }
+
+    public void configureResultScreen(){
+        ResultScreen resultScreen = new ResultScreen();
+        calculatorService.addObserver(resultScreen);
+        screenPanel.add(resultScreen.getLabel());
+    }
+
+    public void configureInputScreen(){
+        InputScreen inputScreen = new InputScreen();
+        calculatorService.addObserver(inputScreen);
+        screenPanel.add(inputScreen.getLabel());
     }
 }
